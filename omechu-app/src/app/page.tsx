84ec704c2e538652d3d4b336dba8f505c9d8de96
2ssac <ivory.ma9ic@gmail.com>
2025-06-30 13:52:43 +0900

    feat:next-setup #1

---
+++ omechu-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="flex items-center flex-1 h-screen font-bold text-center text-orange-700">
+      Hello, Omechu!
+    </div>
+  );
+}
