db4445821063f91e7cb65b354637c043de7b1291
2ssac <ivory.ma9ic@gmail.com>
2025-07-03 00:00:13 +0900

    design: Header 스타일 조정 #15

--- omechu-app/src/app/components/common/Header.tsx
+++ omechu-app/src/app/components/common/Header.tsx
@@ -6,7 +6,16 @@ type HeaderProps = {
 
 const Header = ({ title, leftChild, rightChild }: HeaderProps) => {
   return (
-    <header className="flex items-center px-3.5 py-2.5 w-full h-11 border-b-[1px] border-[#828282]">
+    <header
+      className="flex items-center px-5 py-2.5 w-full border-b border-[#828282] hover:bg-[#dfc0e6]"
+      style={{
+        paddingTop:
+          typeof window !== "undefined" &&
+          /iPhone|iPad|iPod|Macintosh/.test(navigator.userAgent)
+            ? "env(safe-area-inset-top)"
+            : "0.625rem",
+      }} // 이 부분은 아이폰일 때 상단 노치 관련 paddingTop 조정하는 부분
+    >
       <div className="flex justify-start w-1/4">{leftChild}</div>
       <div className="flex justify-center w-1/2 text-xl font-medium">
         {title}
