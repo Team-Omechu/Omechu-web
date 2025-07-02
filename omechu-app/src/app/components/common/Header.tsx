e5dc8bd192c1e75c6e87620bad675b1769640f7a
2ssac <ivory.ma9ic@gmail.com>
2025-07-03 04:19:38 +0900

    design: header className  props 추가 #16

--- omechu-app/src/app/components/common/Header.tsx
+++ omechu-app/src/app/components/common/Header.tsx
@@ -2,11 +2,14 @@ type HeaderProps = {
   title?: React.ReactNode;
   leftChild?: React.ReactNode;
   rightChild?: React.ReactNode;
+  className?: string;
 };
 
-const Header = ({ title, leftChild, rightChild }: HeaderProps) => {
+const Header = ({ title, leftChild, rightChild, className }: HeaderProps) => {
   return (
-    <header className="flex items-center px-5 py-2.5 w-full border-b border-[#828282] hover:bg-[#dfc0e6]">
+    <header
+      className={`flex items-center px-5 py-2.5 w-full border-b border-[#828282] hover:bg-[#dfc0e6] ${className}`}
+    >
       <div className="flex justify-start w-1/4">{leftChild}</div>
       <div className="flex justify-center w-1/2 text-xl font-medium">
         {title}
