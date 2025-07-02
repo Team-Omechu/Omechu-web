32ffa95a778af86dc87aef926642b5da997ccb2d
2ssac <ivory.ma9ic@gmail.com>
2025-07-02 12:04:50 +0900

    style: layout.tsx에  mobile-screen view 적용 #9

--- omechu-app/src/app/layout.tsx
+++ omechu-app/src/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import BottomNav from "./components/common/Bottom";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,14 +21,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-        <BottomNav />
+    <html lang="ko">
+      <body className="flex justify-center bg-[#f8f9fa] min-h-screen overflow-x-hidden">
+        <div className="relative w-full min-h-screen bg-white shadow-md max-w-screen-mobile">
+          {children}
+          <BottomNav />
+        </div>
       </body>
     </html>
   );
