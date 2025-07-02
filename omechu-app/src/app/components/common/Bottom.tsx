316ac8591ce0f39615726af599c3c14425bf00af
2ssac <ivory.ma9ic@gmail.com>
2025-07-02 13:48:24 +0900

    feat:BottomNav router 추가 #14

--- omechu-app/src/app/components/common/Bottom.tsx
+++ omechu-app/src/app/components/common/Bottom.tsx
@@ -1,18 +1,37 @@
+"use client";
 import Image from "next/image"; // Next.js에서 제공하는 next/image 컴포넌트 사용
+import { useRouter } from "next/navigation";
 
 const BottomNav = () => {
+  const router = useRouter(); // React의 useNavigator처럼 변수 선언
+
   return (
     <div className="relative bottom-0 left-0 right-0 h-20 bg-white max-w-screen-mobile rounded-t-[10px] pb-[env(safe-area-inset-bottom)] shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]">
       <div className="flex justify-between w-full px-5 py-2.5">
-        <div className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100">
+        <div
+          onClick={() => {
+            router.push("/");
+          }}
+          className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100"
+        >
           <Image src="/Home.png" alt="Home" width={26} height={26} />
           <span className="text-xs font-medium">홈</span>
         </div>
-        <div className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100">
+        <div
+          onClick={() => {
+            router.push("/");
+          }}
+          className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100"
+        >
           <Image src="/menu.png" alt="menu" width={26} height={26} />
           <span className="text-xs font-medium">전체메뉴</span>
         </div>
-        <div className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100">
+        <div
+          onClick={() => {
+            router.push("/");
+          }}
+          className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100"
+        >
           <Image
             src="/restaurant.png"
             alt="restaurant"
@@ -21,7 +40,12 @@ const BottomNav = () => {
           />
           <span className="text-xs font-medium">맛집</span>
         </div>
-        <div className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100">
+        <div
+          onClick={() => {
+            router.push("/mypage");
+          }} // 아직 mypage 구현 전 -> div 눌러도 이동 X
+          className="flex flex-col items-center justify-center gap-1 py-1 rounded-lg cursor-pointer w-14 hover:bg-gray-100"
+        >
           <Image src="/mypage.png" alt="mypage" width={26} height={26} />
           <span className="text-xs font-medium">마이페이지</span>
         </div>
