d7b452c7ddd18ec97e1ccf0d8a52bd2b74a4734e
2ssac <ivory.ma9ic@gmail.com>
2025-07-03 04:17:42 +0900

    design: header safe area(아이폰 상단 노치) 관련 클래스 제거

--- omechu-app/src/app/components/common/Header.tsx
+++ omechu-app/src/app/components/common/Header.tsx
@@ -6,16 +6,7 @@ type HeaderProps = {
 
 const Header = ({ title, leftChild, rightChild }: HeaderProps) => {
   return (
-    <header
-      className="flex items-center px-5 py-2.5 w-full border-b border-[#828282] hover:bg-[#dfc0e6]"
-      style={{
-        paddingTop:
-          typeof window !== "undefined" &&
-          /iPhone|iPad|iPod|Macintosh/.test(navigator.userAgent)
-            ? "env(safe-area-inset-top)"
-            : "0.625rem",
-      }} // 이 부분은 아이폰일 때 상단 노치 관련 paddingTop 조정하는 부분
-    >
+    <header className="flex items-center px-5 py-2.5 w-full border-b border-[#828282] hover:bg-[#dfc0e6]">
       <div className="flex justify-start w-1/4">{leftChild}</div>
       <div className="flex justify-center w-1/2 text-xl font-medium">
         {title}
