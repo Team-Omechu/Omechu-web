6281291bb0b8a6fafdb00ad379382700f2df05a6
2ssac <ivory.ma9ic@gmail.com>
2025-07-03 03:54:34 +0900

    feat/mypage 화면 구현 #16

--- omechu-app/src/app/components/common/Header.tsx
+++ omechu-app/src/app/components/common/Header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 type HeaderProps = {
   title?: React.ReactNode;
   leftChild?: React.ReactNode;
@@ -8,7 +6,16 @@ type HeaderProps = {
 
 const Header = ({ title, leftChild, rightChild }: HeaderProps) => {
   return (
-    <header className="flex items-center px-5 py-2.5 w-full border-b border-[#828282] hover:bg-[#dfc0e6]">
+    <header
+      className="flex items-center px-5 py-2.5 w-full border-b border-[#828282] hover:bg-[#dfc0e6]"
+      style={{
+        paddingTop:
+          typeof window !== "undefined" &&
+          /iPhone|iPad|iPod|Macintosh/.test(navigator.userAgent)
+            ? "env(safe-area-inset-top)"
+            : "0.625rem",
+      }} // 이 부분은 아이폰일 때 상단 노치 관련 paddingTop 조정하는 부분
+    >
       <div className="flex justify-start w-1/4">{leftChild}</div>
       <div className="flex justify-center w-1/2 text-xl font-medium">
         {title}
