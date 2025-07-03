322a0a21efbce30d1763aa42632edf271357e6e4
2ssac <ivory.ma9ic@gmail.com>
2025-07-04 02:05:08 +0900

    refactor:ProgressBar 재사용을 위한 Props 추가 및 로직 개선 #21

--- omechu-app/src/app/mypage/user-info-setup/gender/page.tsx
+++ omechu-app/src/app/mypage/user-info-setup/gender/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import ProgressBar from "@/app/components/common/ProgressBar";
 import { useRouter } from "next/navigation";
+import { userInfoStepMap } from "@/app/constants/stepMap";
 
 export default function SetupGender() {
   const router = useRouter();
   return (
     <div className="flex flex-col w-auto h-screen">
-      <ProgressBar />
+      <ProgressBar stepMap={userInfoStepMap} />
       <main className="flex flex-col items-center justify-center flex-1 w-full h-full gap-12">
         <section>
           <div className="text-3xl font-medium">성별은 무엇인가요?</div>
--- omechu-app/src/app/mypage/user-info-setup/state/page.tsx
+++ omechu-app/src/app/mypage/user-info-setup/state/page.tsx
@@ -2,12 +2,13 @@
 
 import ProgressBar from "@/app/components/common/ProgressBar";
 import { useRouter } from "next/navigation";
+import { userInfoStepMap } from "@/app/constants/stepMap";
 
 export default function SetupState() {
   const router = useRouter();
   return (
     <div className="flex flex-col w-auto h-screen">
-      <ProgressBar />
+      <ProgressBar stepMap={userInfoStepMap} />
       <main className="flex flex-col items-center justify-center flex-1 w-full h-full gap-12">
         <section>
           <div className="px-10 text-3xl font-medium leading-relaxed text-center whitespace-pre">
