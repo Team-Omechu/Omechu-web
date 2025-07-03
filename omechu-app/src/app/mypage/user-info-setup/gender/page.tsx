38cf018b8dc554e1051af19e55f7174ba60edab6
2ssac <ivory.ma9ic@gmail.com>
2025-07-03 13:59:14 +0900

    wip feat/user-info-setup/gender 임시 저장

---
+++ omechu-app/src/app/mypage/user-info-setup/gender/page.tsx
@@ -0,0 +1,10 @@
+export default function SetupGender() {
+  return (
+    <div className="relative ">
+      <main></main>
+      <footer className="absolute bottom-0">
+        <button className=" bg-[#1F9BDA]">다음</button>
+      </footer>
+    </div>
+  );
+}
