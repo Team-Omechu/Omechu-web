27221927a123dae87294d551243f1878421bdded
2ssac <ivory.ma9ic@gmail.com>
2025-07-03 15:06:51 +0900

    feat:기본상태입력-성별 화면 구현 9

--- omechu-app/src/app/mypage/user-info-setup/gender/page.tsx
+++ omechu-app/src/app/mypage/user-info-setup/gender/page.tsx
@@ -1,9 +1,39 @@
+"use client";
+import Header from "@/app/components/common/Header";
+import { useRouter } from "next/navigation";
+
 export default function SetupGender() {
+  const router = useRouter();
   return (
-    <div className="relative ">
-      <main></main>
-      <footer className="absolute bottom-0">
-        <button className=" bg-[#1F9BDA]">다음</button>
+    <div className="flex flex-col w-auto h-screen">
+      <Header title={"progress-bar"} />
+      <main className="flex flex-col items-center justify-center flex-1 w-full h-full gap-12">
+        <section>
+          <div className="text-3xl font-medium">성별은 무엇인가요?</div>
+        </section>
+        <section>
+          <div className="flex gap-5">
+            <button className="w-28 h-14 p-2.5 bg-white border-[1px] rounded-md border-[#FB4746] text-xl text-[#FB4746]">
+              여성
+            </button>
+            <button className="w-28 h-14 p-2.5 bg-white border-[1px] rounded-md border-[#FB4746] text-xl text-[#FB4746]">
+              남성
+            </button>
+          </div>
+        </section>
+      </main>
+      <footer className="flex flex-col items-end w-full pb-[env(safe-area-inset-bottom)] gap-3">
+        <button
+          onClick={() => {
+            router.push("./state");
+          }}
+          className="mr-5 text-base text-[#828282]"
+        >
+          건너뛰기 {">"}
+        </button>
+        <button className="p-2 min-w-full h-12 text-white text-xl font-normal rounded-t-md bg-[#1f9bda] hover:bg-[#1c8cc4] active:bg-[#197cae]">
+          다음
+        </button>
       </footer>
     </div>
   );
