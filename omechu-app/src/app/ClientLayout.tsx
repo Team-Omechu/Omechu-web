2fae87dc48a376f5e01920e0d543d4cc6c03cfab
2ssac <ivory.ma9ic@gmail.com>
2025-07-03 14:07:43 +0900

    refactor:ClienteLayout.tsx 생성 및 layout.tsx 리팩토링 #18

---
+++ omechu-app/src/app/ClientLayout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import BottomNav from "./components/common/Bottom";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  const noBottomNavRoutes = [
+    "/mypage/user-info-setup",
+    "/mypage/user-info-setup/gender",
+    "/mypage/profile-edit",
+  ];
+  const showBottomNav = !noBottomNavRoutes.includes(pathname);
+
+  return (
+    <>
+      <main
+        className={`${showBottomNav ? "pb-20" : ""} flex-1 bg-[#F8D5FF] overflow-y-scroll`}
+      >
+        {children}
+      </main>
+      {showBottomNav && <BottomNav />}
+    </>
+  );
+}
