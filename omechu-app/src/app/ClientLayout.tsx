b86c47aa43a06b785bf95600da1ce0f334bd0eed
jeonbinggu <tinyjbk123@gmail.com>
2025-07-04 21:32:51 +0900

    refactor: ClientLayout noBottomNav에 추가 경로 작성

--- omechu-app/src/app/ClientLayout.tsx
+++ omechu-app/src/app/ClientLayout.tsx
@@ -19,6 +19,12 @@ export default function ClientLayout({
     "/mypage/user-info-setup/food",
     "/mypage/user-info-setup/condition",
     "/mypage/user-info-setup/allergy",
+    "/mainpage/question-answer/meal-time",
+    "/mainpage/question-answer/purpose",
+    "/mainpage/question-answer/state",
+    "/mainpage/question-answer/who",
+    "/mainpage/question-answer/budget",
+    "/mainpage/question-answer/middle-question",
   ];
   const showBottomNav = !noBottomNavRoutes.includes(pathname);
 
