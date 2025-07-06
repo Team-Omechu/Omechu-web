ecd7222ec49eb4a274f438a81f1c31685db4e2aa
2ssac <ivory.ma9ic@gmail.com>
2025-07-06 23:22:03 +0900

    design:AlertModal 다크모드 구현

--- omechu-app/src/app/components/common/AlertModal.tsx
+++ omechu-app/src/app/components/common/AlertModal.tsx
@@ -18,12 +18,16 @@ export default function AlertModal({
 }: AlertModalProps) {
   const showCancelButton = !!cancelText;
   return (
-    <div className="w-[335px] h-[200px] bg-white rounded-[20px] p-5 flex flex-col justify-between shadow-xl">
+    <div
+      className="w-[335px] h-[180px] flex flex-col justify-between p-5 
+                    shadow-xl rounded-[20px]
+                    bg-white dark:bg-[#a3a3a3]"
+    >
       <div className="flex justify-end"></div>
       <div className="flex flex-col items-center text-center">
         <span className="text-[19px] font-medium">{title}</span>
         {description && (
-          <span className="mt-1 text-[15px] text-[#828282] font-medium">
+          <span className="mt-1 text-[15px] font-medium text-[#828282] dark:text-white">
             {description}
           </span>
         )}
@@ -31,14 +35,22 @@ export default function AlertModal({
       <div className="flex justify-center gap-2.5">
         <button
           onClick={onConfirm}
-          className={`h-10 rounded-[30px] text-white bg-[#FB4746] hover:bg-[#e2403f] active:bg-[#c93938] text-[15px] font-normal ${showCancelButton ? "flex-1" : "w-40"}`}
+          className={`h-10 rounded-[30px] text-white text-[15px] font-normal
+                    bg-[#FB4746] dark:bg-[#bc3535]
+                    hover:bg-[#e2403f] dark:hover:bg-[#972b2a]
+                    active:bg-[#c93938] dark:active:bg-[#71201f]
+                    ${showCancelButton ? "flex-1" : "w-40"}`}
         >
           {confirmText}
         </button>
         {showCancelButton && (
           <button
             onClick={onClose}
-            className="flex-1 h-10 rounded-[30px] bg-white border border-black hover:bg-[#f1f1f1] active:bg-[#e2e2e2] text-[15px] font-normal"
+            className="flex-1 h-10 text-[15px] font-normal
+                      rounded-[30px] border-black border-2 dark:border-none
+                      bg-white dark:bg-[#7a7a7a]
+                      hover:bg-[#f1f1f1] dark:hover:bg-[#626262]
+                      active:bg-[#e2e2e2] dark:active:bg-[#494949]"
           >
             {cancelText}
           </button>
