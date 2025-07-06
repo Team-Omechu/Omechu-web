85c11107f85b1f71a8069475e03868075b26c1b8
2ssac <ivory.ma9ic@gmail.com>
2025-07-06 23:40:48 +0900

    design:기본정보입력하기창 다크모드 구현

--- omechu-app/src/app/mypage/user-info-setup/page.tsx
+++ omechu-app/src/app/mypage/user-info-setup/page.tsx
@@ -15,10 +15,10 @@ export default function UserInfoSetup() {
           <button onClick={() => router.push("./user-info-edit")}>{"<"}</button>
         }
       />{" "}
-      <main className="flex flex-col items-center justify-center w-full gap-24 px-4 py-6 min-h-[calc(100vh-10rem)]">
-        <section className="flex flex-col gap-5 text-center">
+      <main className="flex flex-col items-center w-full px-4 overflow-y-scroll min-h-dvh scroll-smooth overscroll-none">
+        <section className="flex flex-col gap-5 mt-32 mb-24 text-center">
           <div className="text-2xl font-medium">기본 정보 입력하기</div>
-          <span className="whitespace-pre font-normal text-[#828282]">
+          <span className="whitespace-pre font-normal text-[#828282] dark:text-[#fffcfc]">
             기본정보를 저장하여 {"\n"} 더 정교한 메뉴 추천을 받아보세요!
           </span>
           {/* prettier formatOnSave 때문에 whitespace-pre 미적용 -> 추후 수정 */}
@@ -26,7 +26,11 @@ export default function UserInfoSetup() {
         <section>
           <button
             onClick={() => router.push("user-info-setup/gender")}
-            className="w-48 h-16 p-5 bg-[#fb4746] hover:bg-[#e2403f] rounded-md active:bg-[#c93938] text-white text-2xl flex items-center justify-center font-medium"
+            className="w-48 h-16 p-5 flex items-center justify-center
+                      text-white text-2xl font-medium rounded-md 
+                      bg-[#FB4746] dark:bg-[#bc3535]
+                      hover:bg-[#e2403f] dark:hover:bg-[#972b2a]
+                      active:bg-[#c93938] dark:active:bg-[#71201f]"
           >
             시작하기
           </button>
