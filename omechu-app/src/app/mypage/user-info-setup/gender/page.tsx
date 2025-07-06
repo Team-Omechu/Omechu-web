796af450cc639b9196fa3e2d8426c6ce146ef2b9
2ssac <ivory.ma9ic@gmail.com>
2025-07-06 16:45:19 +0900

    designAlertModal에 버튼 1개일때 동작 추가

--- omechu-app/src/app/mypage/user-info-setup/gender/page.tsx
+++ omechu-app/src/app/mypage/user-info-setup/gender/page.tsx
@@ -57,12 +57,10 @@ export default function SetupGender() {
             title="기본 상태 입력을 중단하시겠어요?"
             description="지금까지 작성한 내용은 저장되지 않아요."
             confirmText="그만하기"
-            cancelText="돌아가기"
             onConfirm={() => {
               setShowModal(false);
               router.push("./"); // 원하는 페이지로 이동
             }}
-            onClose={() => setShowModal(false)}
           />
         </ModalWrapper>
       )}
