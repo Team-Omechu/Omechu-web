860414570946db7907d7e3e44a8f1fba81f181f8
2ssac <ivory.ma9ic@gmail.com>
2025-07-07 15:56:46 +0900

    FoodBox에 imgUrl(fallback) Props 추가

--- omechu-app/src/app/components/mypage/FoodBox.tsx
+++ omechu-app/src/app/components/mypage/FoodBox.tsx
@@ -2,11 +2,17 @@ import Image from "next/image";
 
 type FoodBoxProp = {
   title: string;
+  imageUrl?: string | null; // 실제 서비스에서는 이미지 경로 들어옴
   isExcluded: boolean;
   onToggle: () => void;
 };
 
-export default function FoodBox({ title, isExcluded, onToggle }: FoodBoxProp) {
+export default function FoodBox({
+  title,
+  imageUrl,
+  isExcluded,
+  onToggle,
+}: FoodBoxProp) {
   return (
     <div
       className="relative py-1 w-[100px] h-[110px] 
@@ -14,19 +20,8 @@ export default function FoodBox({ title, isExcluded, onToggle }: FoodBoxProp) {
         border-[1px] border-black rounded-xl bg-white 
         hover:scale-110 transition-transform duration-300"
     >
-      {/* <img
-        decoding="async"
-        loading="eager"
-        onClick={() => {
-          console.log("클릭됨", title);
-          onToggle();
-        }}
-        className="absolute cursor-pointer top-1 right-1"
-        src={isExcluded ? `/add_circle.webp` : `/do_not_disturb_on.webp`}
-        alt={isExcluded ? "음식 추천 제외 취소" : "음식 추천 제외"}
-        width={25}
-        height={25}
-      /> */}
+      {/* 추천 제외 || 복원 버튼 */}
+      {/* dev 모드 일 때 이미지 렌더링 늦음 -> build 후 확인하니 렌더링 정상 (캐시 사용) */}
       <Image
         priority
         loading="eager"
@@ -42,7 +37,21 @@ export default function FoodBox({ title, isExcluded, onToggle }: FoodBoxProp) {
         height={25}
         sizes="25px"
       />
-      <div className="bg-gray-200 w-[70px] h-[70px] rounded-lg" />
+
+      {/* 음식 이미지 (또는 fallback) */}
+      {imageUrl ? (
+        <Image
+          src={imageUrl}
+          alt={`${title} 이미지`}
+          width={70}
+          height={70}
+          className="object-cover rounded-lg"
+        />
+      ) : (
+        <div className="bg-gray-200 w-[70px] h-[70px] rounded-lg" />
+      )}
+
+      {/* 음식 이름 */}
       <span className="text-[15px] font-normal">{title}</span>
     </div>
   );
