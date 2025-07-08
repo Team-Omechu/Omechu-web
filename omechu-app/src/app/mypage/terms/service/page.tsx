57290b12406a7c28eb4ac12cf029e9221e6fa48d
2ssac <ivory.ma9ic@gmail.com>
2025-07-08 17:17:27 +0900

    feat:서비스 이용약관 구현 화면 완성

--- omechu-app/src/app/mypage/terms/service/page.tsx
+++ omechu-app/src/app/mypage/terms/service/page.tsx
@@ -1,12 +1,41 @@
+"use client";
+
+import Header from "@/app/components/common/Header";
+import { useRouter } from "next/navigation";
+
 export default function ServiceTerm() {
+  const router = useRouter();
+
   return (
-    <main className="">
-      <section className="flex flex-col justify-start gap-2">
-        {/* 조항 번호 */}
-        <div className="text-base font-bold"></div>
-        {/* 조항 내용 */}
-        <div className="text-base font-normal"></div>
-      </section>
-    </main>
+    <>
+      <Header
+        title={"서비스 이용 약관"}
+        leftChild={
+          <button
+            onClick={() => {
+              router.push("/mypage/terms");
+            }}
+          >
+            {"<"}
+          </button>
+        }
+      />
+
+      <main className="w-full px-7 py-9 ">
+        <section className="flex flex-col justify-start gap-1 mb-3">
+          {/* 조항 번호 */}
+          <div className="text-base font-bold">제 1조</div>
+          {/* 조항 내용 */}
+          <div className="text-base font-normal text-[#828282] leading-relaxed">
+            내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용
+            내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용
+            내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용
+            내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용
+            내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용
+            내용
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
