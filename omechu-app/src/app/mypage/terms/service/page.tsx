3bcfe122a8718d45d3cbdb1cadaf2b8bd4c7a9d6
2ssac <ivory.ma9ic@gmail.com>
2025-07-08 17:09:06 +0900

    feat:서비스 이용약관 구현 시작

---
+++ omechu-app/src/app/mypage/terms/service/page.tsx
@@ -0,0 +1,12 @@
+export default function ServiceTerm() {
+  return (
+    <main className="">
+      <section className="flex flex-col justify-start gap-2">
+        {/* 조항 번호 */}
+        <div className="text-base font-bold"></div>
+        {/* 조항 내용 */}
+        <div className="text-base font-normal"></div>
+      </section>
+    </main>
+  );
+}
