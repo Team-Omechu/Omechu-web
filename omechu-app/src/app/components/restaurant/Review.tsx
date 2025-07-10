1fa9c428ee599797d054620326c67d6a48e19b99
2ssac <ivory.ma9ic@gmail.com>
2025-07-10 16:27:43 +0900

    맛집-상세 wip 90% #37

--- omechu-app/src/app/components/restaurant/Review.tsx
+++ omechu-app/src/app/components/restaurant/Review.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import Image from "next/image";
 
 type ReviewProps = {
+  id: number;
   profileImgUrl: string;
   userId: string;
   createdDate: string;
@@ -10,9 +10,12 @@ type ReviewProps = {
   content: string;
   tags?: string[];
   images?: string[];
+  onClick?: () => void;
+  isOptionOpen?: boolean;
 };
 
 export default function Review({
+  id,
   profileImgUrl,
   userId,
   createdDate,
@@ -21,10 +24,12 @@ export default function Review({
   content,
   tags,
   images,
+  onClick,
+  isOptionOpen = false,
 }: ReviewProps) {
   return (
     <section
-      className="w-full h-fit p-4 flex flex-col gap-3 bg-white 
+      className="w-full h-fit p-4 flex flex-col gap-2 bg-white 
                 border-[1px] border-[#393939] rounded-xl "
     >
       {/* 프로필사진, 닉네임, 작성일 | 추천 버튼, 설정버튼 */}
@@ -53,7 +58,7 @@ export default function Review({
               height={22}
             />
           </button>
-          <button>
+          <button onClick={onClick}>
             <Image
               src="/menu_dots_vertical.png"
               alt="메뉴"
@@ -62,29 +67,44 @@ export default function Review({
             />
           </button>
         </div>
+        {isOptionOpen && (
+          <div className="absolute p-2 bg-white border rounded shadow -right-1 top-6">
+            <button className="flex items-center justify-center w-full gap-2 text-left">
+              <Image
+                src="/report.png"
+                alt={"신고하기"}
+                width={13}
+                height={18}
+              />
+              <span className="mt-1">신고하기</span>
+            </button>
+          </div>
+        )}
       </div>
       {/* 평점, 후기 */}
-      <div className="flex flex-col gap-1 ml-2">
-        <span className="text-2xl font-normal text-[#1F9BDA]">
+      <div className="flex flex-col gap-1 mt-1 ml-2">
+        <span className="text-xl font-normal text-[#1F9BDA]">
           {"★".repeat(rating) + "☆".repeat(5 - rating)}
         </span>
         <span className="text-xl font-normal text-[#828282]">{content}</span>
-        <div>
+        <div className="flex flex-shrink-0 gap-2 mt-1 overflow-x-scroll scrollbar-hide scroll-smooth max-h-52">
           {images?.map((item, index) => (
-            <div
-              key={index}
-              className="px-3 pt-1 pb-0.5 bg-white border-[1px] w-fit rounded-3xl border-[#393939] text-sm font-nomal hover:scale-105 duration-300"
-            >
-              {item}
-            </div>
+            <Image
+              key={`review-img-${index}`}
+              src={item}
+              alt={`후기 이미지 ${index + 1}`}
+              width={200}
+              height={200}
+              className="rounded-md shrink-0"
+            />
           ))}
         </div>
       </div>
       {/* 후기 관련 태그 */}
       <div className="flex flex-wrap justify-start gap-1 px-1">
-        {tags?.map((item, index) => (
+        {tags?.map((item, tagIdx) => (
           <div
-            key={index}
+            key={`tag-${id}-${tagIdx}`}
             className="px-3 pt-1 pb-0.5 bg-white border-[1px] w-fit rounded-3xl border-[#393939] text-sm font-nomal hover:scale-105 duration-300"
           >
             {item}
