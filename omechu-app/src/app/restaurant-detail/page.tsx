4c6f5cf975471697d9121fd8ffbec77cbfebf253
2ssac <ivory.ma9ic@gmail.com>
2025-07-12 01:56:18 +0900

    refactor:sampleReviews 상수 파일 분리 #37

--- omechu-app/src/app/restaurant-detail/page.tsx
+++ omechu-app/src/app/restaurant-detail/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { sampleReviews } from "@/app/constant/restaurant/sampleReviews";
+
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -37,55 +39,6 @@ const restaurant_time_table = [
   {
     days_of_the_week: "일",
     time: "11:00 - 19:00",
-  },
-];
-
-const sample_reviews: {
-  id: number;
-  profileImgUrl: string;
-  userId: string;
-  createdDate: string;
-  votes: number;
-  rating: number;
-  content: string;
-  tags?: string[];
-  images?: string[];
-}[] = [
-  {
-    id: 1,
-    profileImgUrl: "/restaurant/avatar1.png",
-    userId: "Alex",
-    createdDate: "2025.05.05",
-    votes: 5,
-    rating: 5,
-    content: "이거 먹으려고 부산에서 왔어요",
-    tags: ["저녁식사", "혼밥", "고급스러운", "가격대 있음"],
-  },
-  {
-    id: 2,
-    profileImgUrl: "/restaurant/avatar2.png",
-    userId: "Keren",
-    createdDate: "2025.05.04",
-    votes: 2,
-    rating: 4,
-    content: "맛있어요",
-    tags: ["점심식사", "데이트"],
-  },
-  {
-    id: 3,
-    profileImgUrl: "/restaurant/avatar3.png",
-    userId: "Jack",
-    createdDate: "2025.04.03",
-    votes: 2,
-    rating: 4,
-    content: "먹을만 해요",
-    tags: ["조용한"],
-    images: [
-      "/restaurant_blank.png",
-      "/restaurant_blank.png",
-      "/restaurant_blank.png",
-      "/restaurant_blank.png",
-    ],
   },
 ];
 
@@ -352,7 +305,7 @@ export default function RestaurantDetail() {
 
             {/* 후기 목록 */}
             <div className="flex flex-col gap-4">
-              {sample_reviews.map((item, index) => (
+              {sampleReviews.map((item, index) => (
                 <Review
                   id={item.id}
                   key={index}
