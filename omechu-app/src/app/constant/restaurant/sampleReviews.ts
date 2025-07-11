bcf555c50db15e5d3e595d67e2ea32294f271f96
2ssac <ivory.ma9ic@gmail.com>
2025-07-12 01:56:18 +0900

    refactor:sampleReviews 상수 파일 분리 #37

---
+++ omechu-app/src/app/constant/restaurant/sampleReviews.ts
@@ -0,0 +1,39 @@
+// mockData
+export const sampleReviews = [
+  {
+    id: 1,
+    profileImgUrl: "/restaurant/avatar1.png",
+    userId: "Alex",
+    createdDate: "2025.05.05",
+    votes: 5,
+    rating: 5,
+    content: "이거 먹으려고 부산에서 왔어요",
+    tags: ["저녁식사", "혼밥", "고급스러운", "가격대 있음"],
+  },
+  {
+    id: 2,
+    profileImgUrl: "/restaurant/avatar2.png",
+    userId: "Keren",
+    createdDate: "2025.05.04",
+    votes: 2,
+    rating: 4,
+    content: "맛있어요",
+    tags: ["점심식사", "데이트"],
+  },
+  {
+    id: 3,
+    profileImgUrl: "/restaurant/avatar3.png",
+    userId: "Jack",
+    createdDate: "2025.04.03",
+    votes: 2,
+    rating: 4,
+    content: "먹을만 해요",
+    tags: ["조용한"],
+    images: [
+      "/restaurant_blank.png",
+      "/restaurant_blank.png",
+      "/restaurant_blank.png",
+      "/restaurant_blank.png",
+    ],
+  },
+];
