25f6b52c52d7ac525ca2c558bf6eb91a3c747402
2ssac <ivory.ma9ic@gmail.com>
2025-07-13 03:06:55 +0900

    refactor: RestaurantDetail 페이지 클라이언트 컴포넌트로 분리하여 빌드 오류 해결#37

--- omechu-app/src/app/restaurant-detail/page.tsx
+++ omechu-app/src/app/restaurant-detail/page.tsx
@@ -1,8 +1,9 @@
 // app/restaurant-detail/page.tsx
 import dynamic from "next/dynamic";
 
+// 클라이언트 컴포넌트를 SSR 없이 불러오기
 const RestaurantDetailClient = dynamic(
-  () => import("./RestaurantDetailClient"), // 실제 클라이언트 컴포넌트
+  () => import("./RestaurantDetailClient"),
   { ssr: false }
 );
 
