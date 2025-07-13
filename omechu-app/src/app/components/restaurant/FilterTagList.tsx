87cc3313d5d8b652e6e28d423b9f355524730257
Rael <emilyekos@gmail.com>
2025-07-14 02:00:59 +0900

    refactor: FilterTagList 컴포넌트 분리 #36

---
+++ omechu-app/src/app/components/restaurant/FilterTagList.tsx
@@ -0,0 +1,16 @@
+import TagItem from "@/app/components/common/Tag";
+
+type FilterTagListProps = {
+  tags: string[];
+  onRemove: (tag: string) => void;
+};
+
+export default function FilterTagList({ tags, onRemove }: FilterTagListProps) {
+  return (
+    <div className="flex overflow-x-auto whitespace-nowrap gap-2 mx-2 scrollbar-hide">
+      {tags.map((tag, idx) => (
+        <TagItem key={idx} label={tag} onRemove={() => onRemove(tag)} className="px-2" />
+      ))}
+    </div>
+  );
+}
