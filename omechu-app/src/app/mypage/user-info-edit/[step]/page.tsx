11c67692b0443bbba2e111f24fdc805e88d0d47a
2ssac <ivory.ma9ic@gmail.com>
2025-07-14 11:12:05 +0900

    fix: 빌드 오류 개선 wip #54

--- omechu-app/src/app/mypage/user-info-edit/[step]/page.tsx
+++ omechu-app/src/app/mypage/user-info-edit/[step]/page.tsx
@@ -1,16 +1,17 @@
-// "use client";
+export const dynamicParams = true;
 
-// // ✅ generateStaticParams 제거
-// // ✅ dynamicParams 명시
-// export const dynamicParams = true;
-
-// import { notFound } from "next/navigation";
-// import { stepComponents } from "@/app/constant/UserInfoEditSteps";
-
-// export default function StepPage({ params }: { params: { step: string } }) {
-//   const Component = stepComponents[params.step as keyof typeof stepComponents];
-
-//   if (!Component) return notFound();
-
-//   return <Component />;
+// 필요 시
+// export function generateStaticParams() {
+//   return stepOrder.map((step) => ({ step }));
 // }
+
+import { notFound } from "next/navigation";
+import { stepComponents } from "@/app/constant/UserInfoEditSteps";
+
+export default function StepPage({ params }: { params: { step: string } }) {
+  const Component = stepComponents[params.step as keyof typeof stepComponents];
+
+  if (!Component) return notFound();
+
+  return <Component />;
+}
