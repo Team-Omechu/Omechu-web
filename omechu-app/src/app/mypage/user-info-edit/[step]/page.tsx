b6a2d73cbd90b08d65e5f6b6ef234809e95bc751
2ssac <ivory.ma9ic@gmail.com>
2025-07-14 11:10:14 +0900

    fix: 빌드 오류 원인 찾기 위해 일부코드 주석처리 #54

--- omechu-app/src/app/mypage/user-info-edit/[step]/page.tsx
+++ omechu-app/src/app/mypage/user-info-edit/[step]/page.tsx
@@ -1,16 +1,16 @@
-"use client";
+// "use client";
 
-// ✅ generateStaticParams 제거
-// ✅ dynamicParams 명시
-export const dynamicParams = true;
+// // ✅ generateStaticParams 제거
+// // ✅ dynamicParams 명시
+// export const dynamicParams = true;
 
-import { notFound } from "next/navigation";
-import { stepComponents } from "@/app/constant/UserInfoEditSteps";
+// import { notFound } from "next/navigation";
+// import { stepComponents } from "@/app/constant/UserInfoEditSteps";
 
-export default function StepPage({ params }: { params: { step: string } }) {
-  const Component = stepComponents[params.step as keyof typeof stepComponents];
+// export default function StepPage({ params }: { params: { step: string } }) {
+//   const Component = stepComponents[params.step as keyof typeof stepComponents];
 
-  if (!Component) return notFound();
+//   if (!Component) return notFound();
 
-  return <Component />;
-}
+//   return <Component />;
+// }
