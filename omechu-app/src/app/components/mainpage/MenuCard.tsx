90ddfc7d8b089e68d001423bde8a4dd89d799341
김덕환 <126309682+IISweetHeartII@users.noreply.github.com>
2025-07-14 14:59:26 +0900

    refactor: MenuCard 컴포넌트에서 이미지 소스 변경 및 코드 정리

--- omechu-app/src/app/components/mainpage/MenuCard.tsx
+++ omechu-app/src/app/components/mainpage/MenuCard.tsx
@@ -7,18 +7,24 @@ interface MenuCardProps {
   title: string;
   description: string;
   image: string;
-  onClick: () => void
+  onClick: () => void;
 }
 
 export default function MenuCard({ title, description, image }: MenuCardProps) {
   return (
     <div className="mt-3 relative cursor-pointer">
       <div className="bg-white border border-black rounded-md p-3 flex gap-3 items-center">
-      <Image src={"/logo_3d.png"} alt={title} width={80} height={80} className="rounded" />
-      <div className="flex-1">
-        <h3 className="text-[#1F9BDA] font-semibold">{title}</h3>
-        <p className="text-sm text-[#828282] mt-1">{description}</p>
-      </div>
+        <Image
+          src={image}
+          alt={title}
+          width={80}
+          height={80}
+          className="rounded"
+        />
+        <div className="flex-1">
+          <h3 className="text-[#1F9BDA] font-semibold">{title}</h3>
+          <p className="text-sm text-[#828282] mt-1">{description}</p>
+        </div>
       </div>
     </div>
   );
