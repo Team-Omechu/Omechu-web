b38ffe5fce1de41d714544e21f114059973c0c94
jeonbinggu <tinyjbk123@gmail.com>
2025-07-15 17:17:25 +0900

    feat: 음식 타입 버튼 컴포넌트 분리-#65

---
+++ omechu-app/src/app/components/mainpage/MealTypeButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+type Props = {
+  selectedItems: string[];
+  onToggle: (item: string) => void;
+};
+
+const MealTypeGroup = ({ selectedItems, onToggle }: Props) => {
+  const items = ["한식", "중식", "일식", "양식", "기타-종류"];
+
+  const renderLabel = (item: string) => (item.startsWith("기타") ? "그 외" : item);
+  const isDisabled = (item: string) =>
+    !selectedItems.includes(item) && selectedItems.length >= 3;
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center gap-[0.3125rem] self-stretch flex-wrap">
+        {items.map((item) => (
+          <button
+            key={item}
+            className={`flex w-[3.625rem] h-[2.1875rem] justify-center items-center gap-[0.625rem] flex-shrink-0 rounded-[0.3125rem] border border-black text-center leading-normal font-['Noto Sans KR'] text-[0.875rem] font-normal ${
+              selectedItems.includes(item)
+                ? "bg-[#FB4746] text-white"
+                : "bg-white"
+            }`}
+            onClick={() => onToggle(item)}
+            disabled={isDisabled(item)}
+          >
+            {renderLabel(item)}
+          </button>
+        ))}
+      </div>
+      <div className="w-full h-px bg-[#828282] opacity-60" />
+    </div>
+  );
+};
+
+export default MealTypeGroup;
