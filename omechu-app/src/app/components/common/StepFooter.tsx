2214905e90a1b2a4fb474b215dc9a399cbc55bcf
jeonbinggu <tinyjbk123@gmail.com>
2025-07-15 17:15:57 +0900

    feat: 스텝푸터바 공용 컴포넌트 제작-#65

---
+++ omechu-app/src/app/components/common/StepFooter.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+
+type StepFooterProps = {
+  showPrev?: boolean;
+  showNext?: boolean;
+  onPrev?: () => void;
+  onNext?: () => void;
+};
+
+const StepFooter = ({
+  showPrev = false,
+  showNext = false,
+  onPrev,
+  onNext,
+}: StepFooterProps) => {
+  return (
+    <footer className="flex flex-col w-full pb-[env(safe-area-inset-bottom)] gap-3">
+      <div className="flex justify-between">
+        {showPrev ? (
+          <button onClick={onPrev} className="ml-5 text-base text-[#828282]">
+            {"<"} 이전으로
+          </button>
+        ) : (
+          <div />
+        )}
+        {showNext ? (
+          <button onClick={onNext} className="mr-5 text-base text-[#828282]">
+            건너뛰기 {">"}
+          </button>
+        ) : null}
+      </div>
+    </footer>
+  );
+};
+
+export default StepFooter;
