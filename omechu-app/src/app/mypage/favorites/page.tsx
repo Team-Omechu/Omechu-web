7306b84f675bf51f9b49df86b6ad702a1100f698
2ssac <ivory.ma9ic@gmail.com>
2025-07-16 04:29:19 +0900

    feat:FoodCard 컴포넌트 렌더링 #72

--- omechu-app/src/app/mypage/favorites/page.tsx
+++ omechu-app/src/app/mypage/favorites/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
+import { distance } from "fastest-levenshtein";
+
+import FoodCard from "@/app/components/common/FoodCard";
 import Header from "@/app/components/common/Header";
+import { foodItems } from "@/app/constant/restautantFoodList"; // 음식 데이터
 
 export default function Favorites() {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const [visibleCount, setVisibleCount] = useState(8);
+
+  const filteredItems = search.trim()
+    ? foodItems.filter((item) => item.menu.includes(search.trim()))
+    : foodItems;
+  const visibleItems = filteredItems.slice(0, visibleCount);
+
+  const similarItems = foodItems.filter(
+    (item) =>
+      distance(item.menu, search.trim()) <= 2 && // 유사 거리 임계값 조정 가능
+      !item.menu.includes(search.trim()), // 정확 검색에 이미 포함된 건 제외
+  );
+
   return (
     <>
       <Header
@@ -19,10 +44,24 @@ export default function Favorites() {
           </Link>
         }
       />
-      <main>
+      <main className="h-full w-full px-5 pb-8 pt-3">
         {/* 필터 - 최신 순 | 오래된 순 */}
         <section></section>
+
         {/* 찜 목록 */}
+        <section className="flex flex-col gap-3">
+          {visibleItems.map((item, idx) => (
+            <FoodCard
+              key={idx}
+              item={item}
+              onClick={() =>
+                router.push(
+                  `/restaurant/restaurant-detail?menu=${encodeURIComponent(item.menu)}`,
+                )
+              }
+            />
+          ))}
+        </section>
       </main>
     </>
   );
