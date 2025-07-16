3e318584c12ee9fc3f0b73f1ddc204761083de24
2ssac <ivory.ma9ic@gmail.com>
2025-07-17 02:46:16 +0900

    feat:my-activity 구현된 컴포넌트 우선 배치#81

--- omechu-app/src/app/mypage/my-activity/page.tsx
+++ omechu-app/src/app/mypage/my-activity/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import Header from "@/app/components/common/Header";
+import SelectTabBar from "@/app/components/mypage/SelectTabBar";
 
 export default function MyActivity() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [sortOrder, setSortOrder] = useState<"Recommended" | "Latest">(
+    "Recommended",
+  );
+
   return (
     <>
       <Header
@@ -19,7 +29,34 @@ export default function MyActivity() {
           </Link>
         }
       />
-      <main className="min-h-full w-full px-6 pb-8 pt-3"></main>
+      {/* 목록 정렬 탭 */}
+      <SelectTabBar
+        tabs={["리뷰", "등록한 맛집"]}
+        selectedIndex={selectedIndex}
+        onSelect={setSelectedIndex}
+      />
+      <main className="min-h-full w-full px-6 pb-8 pt-3">
+        {/* 필터 - 추천 순 | 최신 순 */}
+        <section className="flex w-full justify-end gap-1 pb-3 pr-1 pt-1 text-sm text-[#828282]">
+          <button
+            className={
+              sortOrder === "Recommended" ? "font-semibold text-[#393939]" : ""
+            }
+            onClick={() => setSortOrder("Recommended")}
+          >
+            추천 순
+          </button>
+          <span>|</span>
+          <button
+            className={
+              sortOrder === "Latest" ? "font-semibold text-[#393939]" : ""
+            }
+            onClick={() => setSortOrder("Latest")}
+          >
+            최신 순
+          </button>
+        </section>
+      </main>
     </>
   );
 }
