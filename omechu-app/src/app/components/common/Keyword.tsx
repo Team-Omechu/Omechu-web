ab47bab2df28e3beeb0edd55a11c4abf5e4087e7
Rael <emilyekos@gmail.com>
2025-07-17 05:45:48 +0900

    feat: Keyword 컴포넌트에 className prop 추가로 스타일 확장 가능하게 개선 #75

--- omechu-app/src/app/components/common/Keyword.tsx
+++ omechu-app/src/app/components/common/Keyword.tsx
@@ -2,13 +2,19 @@ interface KeywordProps {
   label: string;
   selected: boolean;
   onClick: () => void;
+  className?: string;
 }
 
-export default function Keyword({ label, selected, onClick }: KeywordProps) {
+export default function Keyword({
+  label,
+  selected,
+  onClick,
+  className,
+}: KeywordProps) {
   return (
     <button
       onClick={onClick}
-      className={`h-7 w-20 rounded-full border border-gray-400 text-sm ${
+      className={`h-7 w-20 rounded-full border border-gray-400 text-sm ${className} ${
         selected ? "bg-[#FB4746] text-white" : "bg-white text-gray-600"
       }`}
     >
