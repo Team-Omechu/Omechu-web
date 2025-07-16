a5909078608e479de20e80b3f3c08cb8ac04e42b
Rael <emilyekos@gmail.com>
2025-07-16 16:51:07 +0900

    refactor: FilterTagList에서 TagItem에 className props 전달하도록 수정 #61

--- omechu-app/src/app/components/restaurant/FilterTagList.tsx
+++ omechu-app/src/app/components/restaurant/FilterTagList.tsx
@@ -3,9 +3,10 @@ import TagItem from "@/app/components/common/TagItem";
 type FilterTagListProps = {
   tags: string[];
   onRemove: (tag: string) => void;
+  className?: string;
 };
 
-export default function FilterTagList({ tags, onRemove }: FilterTagListProps) {
+export default function FilterTagList({ tags, onRemove, className }: FilterTagListProps) {
   return (
     <div className="mx-2 flex gap-2 overflow-x-auto whitespace-nowrap scrollbar-hide">
       {tags.map((tag, idx) => (
@@ -13,7 +14,7 @@ export default function FilterTagList({ tags, onRemove }: FilterTagListProps) {
           key={idx}
           label={tag}
           onRemove={() => onRemove(tag)}
-          className="px-2"
+          className={`${className}`}
         />
       ))}
     </div>
