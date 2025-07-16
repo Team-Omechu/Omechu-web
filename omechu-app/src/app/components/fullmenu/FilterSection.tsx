84c4b0838f046ba30ab9fdc945ce48ddf2e65d51
Rael <emilyekos@gmail.com>
2025-07-16 17:15:29 +0900

    refactor: 필터 태그 및 버튼 UI를 FilterSection 컴포넌트로 분리 #61

---
+++ omechu-app/src/app/components/fullmenu/FilterSection.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+import FilterTagList from "../restaurant/FilterTagList";
+
+interface Props {
+  tags: string[];
+  onRemove: (tag: string) => void;
+  onOpen: () => void;
+}
+
+export default function FilterSection({ tags, onRemove, onOpen }: Props) {
+    return (
+        <div className="mt-3 flex items-center gap-2">
+            <FilterTagList
+            tags={tags}
+            onRemove={onRemove}
+            className="px-5"
+            />
+            <button className="ml-auto" onClick={onOpen}>
+                <Image
+                    src={"/customselect.png"}
+                    alt="사용자필터"
+                    className="h-8 w-8"
+                    width={32}
+                    height={32}
+                />
+            </button>
+        </div>
+    )
+}
