f68a5617f3eacfd0f325f07b950b7c4f2a6e57e9
Rael <emilyekos@gmail.com>
2025-07-16 17:30:22 +0900

    refactor: MenuDetail 페이지에서 MenuInfo 컴포넌트 활용하도록 구조 개선 #61

--- omechu-app/src/app/fullmenu/menu-detail/page.tsx
+++ omechu-app/src/app/fullmenu/menu-detail/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import Header from "@/app/components/common/Header";
+import MenuInfo from "@/app/components/mainpage/MenuInfoCard";
 
 export default function MenuDetail() {
   return (
@@ -62,36 +63,7 @@ function MenuDetailClient() {
         </div>
 
         <section className="px-4">
-          <h2 className="mb-2 text-base font-semibold">메뉴 정보</h2>
-
-          <div className="rounded-md border border-gray-400 bg-white p-4 text-[13px] leading-6">
-            <p className="mb-1 font-semibold">기본 영양 정보</p>
-            <div className="grid grid-cols-2 gap-y-1 px-2">
-              <span>칼로리</span>
-              <span className="text-right">950 kcal</span>
-              <span>탄수화물</span>
-              <span className="text-right">120 g</span>
-              <span>단백질</span>
-              <span className="text-right">20 g</span>
-              <span>지방</span>
-              <span className="text-right">35 g</span>
-              <span>비타민</span>
-              <span className="text-right">비타민 B군</span>
-            </div>
-
-            <hr className="my-3" />
-
-            <p className="font-semibold">알레르기 유발 성분</p>
-            <p className="mb-2 pl-2">땅콩, 달걀</p>
-
-            <p className="font-semibold">레시피</p>
-            <p
-              className="cursor-pointer pl-2 text-black underline"
-              onClick={handleClick}
-            >
-              보러가기 ▶
-            </p>
-          </div>
+          <MenuInfo onRecipeClick={handleClick} />
         </section>
       </main>
     </>
