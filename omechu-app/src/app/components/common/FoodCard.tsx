a60f518e78e2c23255c45c50d365850686c8950f
Rael <emilyekos@gmail.com>
2025-07-16 15:42:10 +0900

    fix: item.images가 비어있을 때 런타임 오류 방지를 위한 기본 이미지 추가 #61

--- omechu-app/src/app/components/common/FoodCard.tsx
+++ omechu-app/src/app/components/common/FoodCard.tsx
@@ -39,7 +39,7 @@ export default function FoodCard({ item, onClick }: FoodCardProps) {
           <Image src={"/Heart.svg"} alt="하트" width={20} height={20} />
         </button>
         <Image
-          src={item.images[0]}
+          src={item.images?.[0] || "/restaurant_blank.png"}
           alt={item.menu}
           width={70}
           height={70}
