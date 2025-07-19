b608e592608c560089ae87cdd752d659f0343fc4
2ssac <ivory.ma9ic@gmail.com>
2025-07-20 01:41:37 +0900

    design:RestaurantReviewCard 모달 버튼 위치 변경 #81

--- omechu-app/src/app/components/common/RestaurantReviewCard.tsx
+++ omechu-app/src/app/components/common/RestaurantReviewCard.tsx
@@ -202,6 +202,7 @@ export default function RestaurantReviewCard({
                 setShowDeleteModal(false);
               }}
               onClose={() => setShowDeleteModal(false)}
+              swapButtonOrder={true}
             />
           </ModalWrapper>
         )}
