8366273748877dcbf2fbca4281ac286c841e9fc5
2ssac <ivory.ma9ic@gmail.com>
2025-07-24 06:26:50 +0900

    design:Layout.tsx body {max -> min}-w-screen-mobile 변경-#98

--- omechu-app/src/app/layout.tsx
+++ omechu-app/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`relative mx-auto flex max-h-screen max-w-screen-mobile flex-col overflow-x-hidden dark:bg-[#1a1a1a] dark:text-white ${notoSansKR.variable}`}
+        className={`min-w-screen-mobile relative mx-auto flex max-h-screen flex-col overflow-x-hidden ${notoSansKR.variable}`}
       >
         <ClientLayout>{children}</ClientLayout>
       </body>
