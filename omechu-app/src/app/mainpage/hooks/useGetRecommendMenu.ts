2cf0b50fe441f9ed1e2b8949a38cb759fa78c94e
jeonbinggu <tinyjbk123@gmail.com>
2025-07-26 04:13:07 +0900

    refactor: api hook 파일 수정-#96

--- omechu-app/src/app/mainpage/hooks/useGetRecommendMenu.ts
+++ omechu-app/src/app/mainpage/hooks/useGetRecommendMenu.ts
@@ -1,30 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { getRecommendMenu } from "../api/api";
 import { useQuestionAnswerStore } from "@/lib/stores/questionAnswer.store";
-import { RecommendMenuRequest } from "@/constant/mainpage/resultData";
+import {
+  MenuListResponse,
+  RecommendMenuRequest,
+} from "@/constant/mainpage/resultData";
 
 function useGetRecommendMenu() {
-    const {
-        mealTime,
-        purpose,
-        mood,
-        who,
-        budget,
-        exceptions
-    } = useQuestionAnswerStore();
+  const { mealTime, purpose, mood, who, budget, exceptions } =
+    useQuestionAnswerStore();
 
-    const payload: RecommendMenuRequest = {
-        mealTime,
-        purpose,
-        mood,
-        with: who,
-        budget,
-        exceptions,
-    }
-    return useQuery({
-        queryKey:[],
-        queryFn:()=> getRecommendMenu(payload),
-    })
+  const payload: RecommendMenuRequest = {
+    mealTime,
+    purpose,
+    mood,
+    with: who,
+    budget,
+    exceptions,
+  };
+  return useQuery<MenuListResponse>({
+    queryKey: ["recommendMenu"],
+    queryFn: () => getRecommendMenu(payload),
+    staleTime: 1000 * 60 * 5,
+  });
 }
 
 export default useGetRecommendMenu;
