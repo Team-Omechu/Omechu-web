41a89c79c889a1db507b54cee7946cc7df04739f
2ssac <ivory.ma9ic@gmail.com>
2025-07-28 11:46:37 +0900

    design:AlertModal.tsx padding-bottom 조정-#105

--- omechu-app/src/app/components/common/AlertModal.tsx
+++ omechu-app/src/app/components/common/AlertModal.tsx
@@ -49,13 +49,13 @@ export default function AlertModal({
   const showCancelButton = !!cancelText;
 
   return (
-    <div className="flex h-fit w-[22rem] flex-col justify-between rounded-[20px] border-2 border-grey-darker bg-white px-6 pb-9 pt-14 shadow-xl">
+    <div className="flex h-fit w-[22rem] flex-col justify-between rounded-[20px] border-2 border-grey-darker bg-white px-6 pb-5 pt-14 shadow-xl">
       {/* 상단 여백용 (닫기 버튼 등 들어갈 자리, 현재 비어있음) */}
       <div className="flex justify-end"></div>
 
       {/* 타이틀 및 설명 영역 */}
       <div className="flex flex-col items-center text-center">
-        <span className="whitespace-pre-line text-xl font-medium">{title}</span>
+        <span className="text-xl font-medium whitespace-pre-line">{title}</span>
         {description && (
           <span className="mt-1 whitespace-pre-line text-[15px] font-medium text-grey-normalActive">
             {description}
@@ -71,14 +71,14 @@ export default function AlertModal({
             {showCancelButton && (
               <button
                 onClick={onClose}
-                className="h-10 flex-1 rounded-[30px] border-[1px] border-grey-darkHover bg-white text-[15px] font-normal text-grey-darkHover hover:bg-grey-lightHover active:bg-grey-lightActive"
+                className="h-10 flex-1 rounded-[30px] border-[1px] border-grey-darkHover bg-white pt-1 text-[15px] font-normal text-grey-darkHover hover:bg-grey-lightHover active:bg-grey-lightActive"
               >
                 {cancelText}
               </button>
             )}
             <button
               onClick={onConfirm}
-              className={`h-10 rounded-[30px] bg-primary-normal text-[15px] font-normal text-white hover:bg-primary-normalHover active:bg-primary-normalActive ${showCancelButton ? "flex-1" : "w-40"}`}
+              className={`h-10 rounded-[30px] bg-primary-normal pt-1 text-[15px] font-normal text-white hover:bg-primary-normalHover active:bg-primary-normalActive ${showCancelButton ? "flex-1" : "w-40"}`}
             >
               {confirmText}
             </button>
