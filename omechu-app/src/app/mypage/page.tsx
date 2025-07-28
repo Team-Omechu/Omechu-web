64e287858de96ca5730d62a473fa8646a3b090e0
2ssac <ivory.ma9ic@gmail.com>
2025-07-28 09:42:02 +0900

    wip-#105

--- omechu-app/src/app/mypage/page.tsx
+++ omechu-app/src/app/mypage/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useProfile } from "./hooks/useProfile";
 
 import BottomNav from "../components/common/Bottom";
