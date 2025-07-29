cf7c2f03aaaab07403112c937fd21911a0607097
2ssac <ivory.ma9ic@gmail.com>
2025-07-29 18:53:18 +0900

    feat:/mypage-로딩 및 에러 화면 구현-#105

--- omechu-app/src/app/mypage/user-info-edit/page.tsx
+++ omechu-app/src/app/mypage/user-info-edit/page.tsx
@@ -42,8 +42,42 @@ export default function UserInfoEdit() {
     fetchProfile();
   }, [router, userId]);
 
-  if (error) return <div className="text-red-600">{error}</div>;
-  if (!profile) return <div>로딩 중...</div>;
+  if (error)
+    return (
+      <main className="flex min-h-[calc(100dvh-3rem)] w-full items-center justify-center">
+        {/* <div className="px-6 py-8 text-center rounded-lg shadow bg-red-50"> */}
+        <span className="text-base text-red-600">{error}</span>
+        {/* </div> */}
+      </main>
+    );
+  if (!profile)
+    return (
+      <main className="flex min-h-[calc(100dvh-3rem)] w-full items-center justify-center">
+        <div className="flex flex-col items-center gap-4">
+          <svg
+            className="w-8 h-8 animate-spin text-primary-normal"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            />
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+            />
+          </svg>
+          <span className="text-lg font-medium text-gray-500">로딩 중...</span>
+        </div>
+      </main>
+    );
 
   // Zustand 상태 가져오기
   // const nickname = useOnboardingStore((state) => state.nickname);
