0fa75f35f872dbb7bc4b025d8e56573aa82d64fa
김덕환 <126309682+IISweetHeartII@users.noreply.github.com>
2025-07-31 13:31:06 +0900

    refactor: 온보딩 스토어 및 API에서 성별과 운동 상태 타입을 구체화하고, 프로필 이미지 업로드 기능 추가

--- omechu-app/src/app/onboarding/components/WorkoutStatusStep.tsx
+++ omechu-app/src/app/onboarding/components/WorkoutStatusStep.tsx
@@ -7,16 +7,19 @@ import { useOnboardingStore } from "@/lib/stores/onboarding.store";
 
 import OnboardingStepLayout from "./OnboardingStepLayout";
 
+const options = ["다이어트 중", "증량 중", "유지 중"];
+
 const WorkoutStatusStep = () => {
   const { workoutStatus, setWorkoutStatus } = useOnboardingStore();
-  const options = ["다이어트 중", "증량 중", "유지 중"];
 
   return (
     <OnboardingStepLayout title={"지금 어떤 운동 상태에 \n 가까운가요?"}>
       {options.map((option) => (
         <ListButton
           key={option}
-          onClick={() => setWorkoutStatus(option)}
+          onClick={() =>
+            setWorkoutStatus(option as "다이어트 중" | "증량 중" | "유지 중")
+          }
           isSelected={workoutStatus === option}
         >
           {option}
