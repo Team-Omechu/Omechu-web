bd7da060b8299b1e56104dfb7e3bb834eb6cdeac
Rael <emilyekos@gmail.com>
2025-07-31 12:07:42 +0900

    feat: 구글 place api 활용해서 위치 정보 불러오기 #101

--- omechu-app/src/app/restaurant/restaurant-detail/[id]/map/page.tsx
+++ omechu-app/src/app/restaurant/restaurant-detail/[id]/map/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
@@ -10,6 +10,7 @@ import { Restaurants } from "@/constant/restaurant/restaurantList";
 import RestaurantDetailHeader from "@/restaurant/restaurant-detail/[id]/components/RestaurantDetailHeader";
 
 import RestaurantMapPreview from "./components/RestaurantMapPreview";
+import { fetchGooglePlaceInfo } from "../api/googlePlaceInfo";
 
 export default function MapPage() {
   const router = useRouter();
@@ -26,11 +27,27 @@ export default function MapPage() {
   // 좋아요 상태 관리 (임시로 하트 클릭 시 토글)
   const [isLiked, setIsLiked] = useState(false);
 
+  const [location, setLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
   // 하트 클릭 시 좋아요 상태 토글
   const handleLikeClick = (e: React.MouseEvent) => {
     e.stopPropagation();
     setIsLiked((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (!restaurant) return;
+    if (!restaurant.GooglePlaceId) return;
+
+    fetchGooglePlaceInfo(restaurant.GooglePlaceId)
+      .then((res) => setLocation(res.location))
+      .catch((err) => {
+        console.error("위치 정보를 불러오는 데 실패했습니다:", err);
+      });
+  }, [restaurant]);
 
   // 해당 id의 맛집이 없을 경우 예외 처리 (간단한 메시지)
   if (!restaurant) {
@@ -40,10 +57,6 @@ export default function MapPage() {
       </main>
     );
   }
-
-  // 임시로 사용 중인 지도 이미지 (추후 동적으로 처리 예정이면 여기에 조건문 넣기)
-  const latitude = 37.4895246;
-  const longitude = 126.986331;
 
   return (
     <>
@@ -70,11 +83,15 @@ export default function MapPage() {
         />
 
         {/* 지도 이미지 영역 */}
-        <RestaurantMapPreview
-          latitude={latitude}
-          longitude={longitude}
-          name={restaurant.name}
-        />
+        {location ? (
+          <RestaurantMapPreview
+            latitude={location.latitude}
+            longitude={location.longitude}
+            name={restaurant.name}
+          />
+        ) : (
+          <p className="mt-10 text-gray-500">지도 정보를 불러오는 중...</p>
+        )}
       </main>
     </>
   );
