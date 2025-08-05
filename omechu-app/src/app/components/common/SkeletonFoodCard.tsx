7de03b2067f5538e014776cf1ca7468d29ae1def
2ssac <ivory.ma9ic@gmail.com>
2025-08-05 16:23:40 +0900

    design:SkeletonFoodCard 디자인 수정

--- omechu-app/src/app/components/common/SkeletonFoodCard.tsx
+++ omechu-app/src/app/components/common/SkeletonFoodCard.tsx
@@ -1,12 +1,22 @@
 export default function SkeletonFoodCard() {
   return (
-    <div className="flex animate-pulse flex-row gap-4 rounded-lg bg-gray-100 p-4">
-      <div className="h-20 w-20 rounded-md bg-gray-200" />
-      <div className="flex flex-1 flex-col gap-2">
-        <div className="h-4 w-2/3 rounded bg-gray-200" />
-        <div className="h-3 w-1/2 rounded bg-gray-200" />
-        <div className="h-3 w-1/3 rounded bg-gray-200" />
+    <div className="flex flex-row items-start w-full gap-4 p-4 bg-gray-100 rounded-lg animate-pulse">
+      {/* 좌측: 텍스트 영역 */}
+      <div className="flex flex-col flex-1 gap-2">
+        {/* 식당명, 별점, 하트 */}
+        <div className="flex items-center gap-2">
+          <div className="w-32 h-5 bg-gray-200 rounded" /> {/* 식당명 */}
+          <div className="w-10 h-4 bg-gray-200 rounded" />
+          <div className="w-5 h-5 ml-auto bg-gray-200 rounded-full" />
+          {/* 하트 */}
+        </div>
+        {/* 주소 */}
+        <div className="w-1/2 h-4 bg-gray-200 rounded" />
+        {/* 대표메뉴 */}
+        <div className="w-16 h-4 mt-2 bg-gray-200 rounded" />
       </div>
+      {/* 우측: 미리보기 이미지 */}
+      <div className="w-20 h-20 bg-gray-200 rounded-md shrink-0" />
     </div>
   );
 }
