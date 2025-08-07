4edfad93a635f7d56e8938bf937d5ec8c9a69880
Rael <emilyekos@gmail.com>
2025-08-08 08:54:28 +0900

    feat:  google placeId로 주소의 위도 경도 값 불러오는 api #101

--- omechu-app/src/app/restaurant/restaurant-detail/[id]/api/googlePlaceInfo.ts
+++ omechu-app/src/app/restaurant/restaurant-detail/[id]/api/googlePlaceInfo.ts
@@ -1,19 +1,22 @@
-import axios from "axios";
+export async function fetchGooglePlaceInfo(placeId: string) {
+  try {
+    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY;
+    const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=geometry&key=${apiKey}`;
 
-export interface PlaceLocation {
-  latitude: number;
-  longitude: number;
-}
+    const res = await fetch(url);
+    const data = await res.json();
 
-export interface PlaceInfo {
-  formattedAddress: string;
-  location: PlaceLocation;
-  displayName: { text: string; languageCode: string };
-  currentOpeningHours: {
-    weekdayDescriptions: {
-      days_of_the_week: string;
-      time: string;
-    }[];
+    const location = data.result?.geometry?.location;
+
+    if (!location) {
+      throw new Error("Location 정보가 없습니다.");
+    }
+
+    return {
+      location: {
+        latitude: location.lat,
+        longitude: location.lng,
+      },
   };
   } catch (error) {
     console.error("Google Place Details API 오류:", error);
