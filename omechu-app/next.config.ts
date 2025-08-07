39f1a44277231e80014cb249b6ea78b5c333246a
Rael <emilyekos@gmail.com>
2025-08-08 08:50:05 +0900

    config: 사진 경로 허용 #101

--- omechu-app/next.config.ts
+++ omechu-app/next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   /* config options here */
   images: {
-    domains: ["s3.amazonaws.com"],
+    domains: [
+      "omechu-s3-bucket.s3.ap-northeast-2.amazonaws.com",
+      "s3.amazonaws.com",
+    ],
   },
 };
 
