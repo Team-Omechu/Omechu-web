eecbd7c8a975eb732c9982762f99fd2358edae49
2ssac <ivory.ma9ic@gmail.com>
2025-08-09 09:55:07 +0900

    fix:리팩토링 시 코드 오류 수정-#116

--- omechu-app/src/app/mypage/settings/account-settings/page.tsx
+++ omechu-app/src/app/mypage/settings/account-settings/page.tsx
@@ -1,9 +1,4 @@
-import dynamic from "next/dynamic";
-
-const AccountSettingsClient = dynamic(
-  () => import("./AccountSettingsClient"),
-  { ssr: false }, // 서버에서 그릴 필요 없으면 꺼도 됨 (선호에 따라 켜도 무방)
-);
+import AccountSettingsClient from "./AccountSettingsClient";
 
 export default function AccountSettingsPage() {
   return <AccountSettingsClient />;
