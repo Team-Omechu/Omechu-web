9e8f97ec22b60e8971d36f9a627a6568814a55e9
jeonbinggu <tinyjbk123@gmail.com>
2025-08-18 17:03:22 +0900

    refactor: 제외하기 버튼 클릭 시 제외하기 목록에 추가-#161

--- omechu-app/src/app/mainpage/result/page.tsx
+++ omechu-app/src/app/mainpage/result/page.tsx
@@ -18,6 +18,7 @@ import { useAuthStore } from "@/lib/stores/auth.store";
 import LoginPromptModal2 from "../example_testpage/components/LoginPromptModal2";
 import Toast from "@/components/common/Toast";
 import { useQuestionAnswerStore } from "@/lib/stores/questionAnswer.store";
+import { exceptMenu } from "@/mypage/api/recommend";
 
 export default function ResultPage() {
   const router = useRouter();
@@ -88,6 +89,7 @@ export default function ResultPage() {
         prev.filter((menu) => menu.menu !== excludeMenu),
       );
       if (openMenu === excludeMenu) setOpenMenu(null);
+      exceptMenu({menuName:excludeMenu})
     }
     setShowModal(false);
     setExcludeMenu(null);
