a7aa787f5fac5bf415bb9d0aeab845a9187e7fcc
IISweetHeartII <sachi009955@gmail.com>
2025-11-09 16:17:10 +0900

    remove: FSD 마이그레이션에 따른 mypage/settings 관련 파일 삭제

--- omechu-app/src/app/mypage/settings/account-settings/change-password/page.tsx
+++
@@ -1,8 +0,0 @@
-// TODO: [FSD 마이그레이션] 이 파일은 삭제해도 됩니다.
-// 새 위치: src/app/(routes)/settings/account-settings/change-password/page.tsx
-
-import ChangePasswordClient from "./ChangePasswordClient";
-
-export default function ChangePasswordPage() {
-  return <ChangePasswordClient />;
-}
--- omechu-app/src/app/mypage/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-// TODO: [FSD 마이그레이션] 이 파일은 삭제해도 됩니다.
-// 새 위치: src/app/(routes)/settings/page.tsx
-
-// src/app/mypage/settings/page.tsx
-import SettingsClient from "./SettingsClient";
-
-export default function SettingsPage() {
-  return <SettingsClient />;
-}
