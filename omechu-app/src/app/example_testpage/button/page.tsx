2c37b73a6ad5644a0787ff2911ab10c4d821d8b6
2ssac <ivory.ma9ic@gmail.com>
2026-01-05 16:47:24 +0900

    [#210]design:Button 컴포넌트 수정

--- omechu-app/src/app/example_testpage/button/page.tsx
+++ omechu-app/src/app/example_testpage/button/page.tsx
@@ -2,15 +2,23 @@ import { Button } from "@/shared_FSD/ui/button/Button";
 
 export default function ButtonTestPage() {
   return (
-    <div className="flex flex-col gap-6 p-6">
+    <div className="flex flex-col items-center gap-6 p-6">
       <h2 className="text-body-4-medium">Button</h2>
 
-      <Button>기본 버튼</Button>
-      <Button disabled>비활성 기본 버튼</Button>
-      <Button width="verify">인증번호 확인</Button>
-      <Button width="verify" disabled>
-        인증번호 확인
-      </Button>
+      <Button>로그인</Button>
+      <Button disabled>로그인</Button>
+      <div className="flex w-full justify-end gap-2">
+        <Button width="sm">인증번호 확인</Button>
+        <Button width="sm" disabled>
+          인증번호 확인
+        </Button>
+      </div>
+      <div className="flex gap-2">
+        <Button width="md" fontColor="black" bgColor="white">
+          다시 추천
+        </Button>
+        <Button width="md">선택하기</Button>
+      </div>
     </div>
   );
 }
