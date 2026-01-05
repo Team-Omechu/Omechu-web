05d993b3f231fb0e8ecb3e2308ccbaa7154151f4
2ssac <ivory.ma9ic@gmail.com>
2026-01-05 12:40:39 +0900

    [#210]design:OnboardingButton width variant 추가 및 width 수치 조정

--- omechu-app/src/shared_FSD/ui/button/OnboardingButton.tsx
+++ omechu-app/src/shared_FSD/ui/button/OnboardingButton.tsx
@@ -9,8 +9,9 @@ const OnboardingButtonStyles = cva(
     variants: {
       width: {
         xl: "w-[246px] text-body-2-regular",
-        md: "w-[120.5px] text-body-3-regular",
-        sm: "w-[75.67px] text-body-3-regular",
+        md: "w-[200px] text-body-3-regular",
+        sm: "w-[120.5px] text-body-3-regular",
+        xs: "w-[75.67px] text-body-3-regular",
       },
       selected: {
         true: "bg-brand-primary text-brand-secondary border-none",
