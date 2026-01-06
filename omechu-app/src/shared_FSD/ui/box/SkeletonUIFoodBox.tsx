6dd73f986429de955c41527f0aabe67ee2cc3cf0
Issac <ivory.ma9ic@gmail.com>
2026-01-07 00:06:30 +0900

    [#212]remove:불필요하게 구현한 중복된 컴포넌트 파일 삭제

--- omechu-app/src/shared_FSD/ui/box/SkeletonUIFoodBox.tsx
+++ omechu-app/src/shared_FSD/ui/box/SkeletonUIFoodBox.tsx
@@ -3,7 +3,7 @@ export function SkeletonUIFoodBox() {
     <div className="border-grey-dark-hover flex h-[130px] w-[100px] animate-pulse flex-col items-center justify-between rounded-xl border bg-gray-100 py-2">
       <div className="h-[70px] w-[70px] rounded-xl bg-gray-200"></div>
       <div className="h-[15px] w-[50px] bg-gray-200"></div>
-      <div className="h-[10px] w-[30px] bg-gray-200"></div>
+      <div className="bg-gray-200ㄴ h-[10px] w-[30px]"></div>
     </div>
   );
 }
