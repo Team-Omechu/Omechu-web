01c669e7ec02e38c94dddd99be3b96082de1b571
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-12 08:46:10 +0900

    [#220]feat:MypageMain 구현

--- omechu-app/src/app/example-testpage/mypage/page.tsx
+++ omechu-app/src/app/example-testpage/mypage/page.tsx
@@ -1,8 +1,29 @@
 "use client";
 
-import { Header, MypageModal } from "@/shared_FSD/index";
+import { useState } from "react";
 
 export default function MypageMain() {
+  const [userInfo, setUserInfo] = useState(MOCK_USER_INFO);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleCloseModal = () => {
+    setInputValue("");
+    setIsModalOpen(false);
+  };
+
+  const handleSubmitNickname = () => {
+    if (!inputValue.trim()) return;
+
+    // TODO: 닉네임 변경 API
+    setUserInfo((prev) => ({
+      ...prev,
+      name: inputValue,
+    }));
+
+    handleCloseModal();
+  };
+
   return (
     <>
       <Header />
