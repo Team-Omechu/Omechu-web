5f1e3efba8106c7615e1676fa40010f01ccc6199
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-12 08:05:25 +0900

    [#220]feat:MypageModal 구현

--- omechu-app/src/app/example-testpage/mypage/page.tsx
+++ omechu-app/src/app/example-testpage/mypage/page.tsx
@@ -1,7 +1,15 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> e7bc2625 ([#220]feat:MypageModal 구현)
 <<<<<<< HEAD
 //! 26.01.13 작업
+=======
+"use client";
+
+import { Header, MypageModal } from "@/shared_FSD/index";
+>>>>>>> cfa94326 ([#220]feat:MypageModal 구현)
 
 >>>>>>> 7a424794 (fix:변경사항 저장)
 "use client";
@@ -46,8 +54,13 @@ export default function MypageMain() {
 
   return (
     <>
+<<<<<<< HEAD
       <Header title="마이페이지" isRightChild />
 
+=======
+      <Header title="마이페이지" isRightChild={true} />
+<<<<<<< HEAD
+>>>>>>> e7bc2625 ([#220]feat:MypageModal 구현)
       <main className="relative mt-10 flex h-[80dvh] flex-col items-center gap-6 px-5">
         <UserInfoSection
           {...userInfo}
@@ -56,6 +69,15 @@ export default function MypageMain() {
             setIsModalOpen(true);
           }}
         />
+=======
+      <main className="mt-10 flex h-[80dvh] flex-col items-center gap-6 px-5">
+        <MypageModal
+          title={"닉네임 변경"}
+          onLeftButtonClick={() => {}}
+          onRightButtonClick={() => {}}
+        />
+        <UserInfoSection {...MOCK_USER_INFO} />
+>>>>>>> cfa94326 ([#220]feat:MypageModal 구현)
         <SetAlarmSection />
         <CustomerSupportSection />
       </main>
