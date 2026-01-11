a824c7e03051defbd93ee0c1ce7dc07ace6db649
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-12 04:45:58 +0900

    fix:변경사항 저장

--- omechu-app/src/app/example-testpage/mypage/page.tsx
+++ omechu-app/src/app/example-testpage/mypage/page.tsx
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+//! 26.01.13 작업
+
+>>>>>>> 54b356d0 (fix:변경사항 저장)
 "use client";
 
 import { useState } from "react";
@@ -26,7 +32,43 @@ export default function MypageMain() {
 
   return (
     <>
+<<<<<<< HEAD
       <Header />
+=======
+      <Header title="마이페이지" isRightChild={true} />
+      <main className="relative mt-10 flex h-[80dvh] flex-col items-center gap-6 px-5">
+        <UserInfoSection
+          onNicknameClick={() => {
+            setInputValue(userInfo.name);
+            setIsModalOpen(true);
+          }}
+          {...userInfo}
+        />
+        <SetAlarmSection />
+        <CustomerSupportSection />
+      </main>
+
+      {isModalOpen && (
+        <ModalWrapper className="pb-52" onClose={handleCloseModal}>
+          <MypageModal
+            title={"닉네임 변경"}
+            placeholder={userInfo.name}
+            onChangeInput={(value) => setInputValue(value)}
+            inputValue={inputValue}
+            onLeftButtonClick={handleCloseModal}
+            onRightButtonClick={handleSubmitNickname}
+          />
+        </ModalWrapper>
+      )}
+=======
+import { Header } from "@/shared_FSD/index";
+
+export default function MypageMain() {
+  return (
+    <>
+      <Header />
+>>>>>>> 36422d58 (fix:변경사항 저장)
+>>>>>>> 54b356d0 (fix:변경사항 저장)
     </>
   );
 }
