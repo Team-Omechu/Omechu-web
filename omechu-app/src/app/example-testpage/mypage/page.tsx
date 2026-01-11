5d844715de79322f087ac2a5a942e39e3912ba8c
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-12 04:56:03 +0900

    [#220]chore:mypageMain 레이아웃 설정

--- omechu-app/src/app/example-testpage/mypage/page.tsx
+++ omechu-app/src/app/example-testpage/mypage/page.tsx
@@ -4,6 +4,7 @@ export default function MypageMain() {
   return (
     <>
       <Header title="마이페이지" isRightChild={true} />
+      <main className="mt-10 h-[80dvh] border border-black px-5"></main>
     </>
   );
 }
