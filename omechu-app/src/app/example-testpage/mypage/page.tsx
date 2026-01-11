890dbef082c832e603864379cac2d4bcec631b0e
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-12 06:40:28 +0900

    [#220]feat:SetAlarmCard 컴포넌트 구현 wip

--- omechu-app/src/app/example-testpage/mypage/page.tsx
+++ omechu-app/src/app/example-testpage/mypage/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/shared_FSD/index";
 
+import SetAlarmCard from "./ui/SetAlarmCard";
 import UserInfoCard from "./ui/UserInfoCard";
 
 const MOCK_USER_INFO = {
@@ -13,8 +14,9 @@ export default function MypageMain() {
   return (
     <>
       <Header title="마이페이지" isRightChild={true} />
-      <main className="mt-10 flex h-[80dvh] flex-col items-center border border-black px-5">
+      <main className="mt-10 flex h-[80dvh] flex-col items-center gap-4 border border-black px-5">
         <UserInfoCard {...MOCK_USER_INFO} />
+        <SetAlarmCard />
       </main>
     </>
   );
