b886ec3e307395cee0451e72f7e09024fd75d4d2
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-12 08:46:10 +0900

    [#220]feat:MypageMain 구현

--- omechu-app/src/app/example-testpage/mypage/page.tsx
+++ omechu-app/src/app/example-testpage/mypage/page.tsx
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> f82ba1e4 ([#220]feat:MypageModal 구현)
-<<<<<<< HEAD
 //! 26.01.13 작업
-=======
-"use client";
 
-<<<<<<< HEAD
-import { Header, MypageModal } from "@/shared_FSD/index";
->>>>>>> cfa94326 ([#220]feat:MypageModal 구현)
-
->>>>>>> 54b356d0 (fix:변경사항 저장)
 "use client";
 
 import { useState } from "react";
 
-<<<<<<< HEAD
-=======
-=======
-import { useState } from "react";
-
->>>>>>> 4455f176 ([#220]feat:MypageMain 구현)
-import { Header, ModalWrapper, MypageModal } from "@/shared_FSD/index";
-
-import CustomerSupportSection from "./ui/CustomerSupportSection";
-import SetAlarmSection from "./ui/SetAlarmSection";
-import UserInfoSection from "./ui/UserInfoSection";
+import { Header, ModalWrapper, MypageModal } from "@/shared";
+import {
+  CustomerSupportSection,
+  SetAlarmSection,
+  UserInfoSection,
+} from "@/widgets/mypage/ui";
 
 const MOCK_USER_INFO = {
   name: "제나",
@@ -36,7 +18,6 @@ const MOCK_USER_INFO = {
   allergy: "갑각류",
 };
 
->>>>>>> abc167b9 ([#220]feat:MypageMain 구현)
 export default function MypageMain() {
   const [userInfo, setUserInfo] = useState(MOCK_USER_INFO);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -61,35 +42,17 @@ export default function MypageMain() {
 
   return (
     <>
-<<<<<<< HEAD
-      <Header />
-=======
-      <Header title="마이페이지" isRightChild={true} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4455f176 ([#220]feat:MypageMain 구현)
+      <Header title="마이페이지" isRightChild />
+
       <main className="relative mt-10 flex h-[80dvh] flex-col items-center gap-6 px-5">
         <UserInfoSection
+          {...userInfo}
           onNicknameClick={() => {
             setInputValue(userInfo.name);
             setIsModalOpen(true);
           }}
-          {...userInfo}
-<<<<<<< HEAD
         />
-=======
-      <main className="mt-10 flex h-[80dvh] flex-col items-center gap-6 px-5">
-        <MypageModal
-          title={"닉네임 변경"}
-          onLeftButtonClick={() => {}}
-          onRightButtonClick={() => {}}
-        />
-        <UserInfoSection {...MOCK_USER_INFO} />
->>>>>>> cfa94326 ([#220]feat:MypageModal 구현)
-=======
-        />
->>>>>>> 4455f176 ([#220]feat:MypageMain 구현)
+
         <SetAlarmSection />
         <CustomerSupportSection />
       </main>
@@ -97,30 +60,15 @@ export default function MypageMain() {
       {isModalOpen && (
         <ModalWrapper className="pb-52" onClose={handleCloseModal}>
           <MypageModal
-            title={"닉네임 변경"}
+            title="닉네임 변경"
             placeholder={userInfo.name}
-            onChangeInput={(value) => setInputValue(value)}
             inputValue={inputValue}
+            onChangeInput={setInputValue}
             onLeftButtonClick={handleCloseModal}
             onRightButtonClick={handleSubmitNickname}
           />
         </ModalWrapper>
       )}
-<<<<<<< HEAD
-=======
-import { Header } from "@/shared_FSD/index";
-
-export default function MypageMain() {
-  return (
-    <>
-      <Header />
->>>>>>> 36422d58 (fix:변경사항 저장)
-<<<<<<< HEAD
->>>>>>> 54b356d0 (fix:변경사항 저장)
-=======
-=======
->>>>>>> 4455f176 ([#220]feat:MypageMain 구현)
->>>>>>> abc167b9 ([#220]feat:MypageMain 구현)
     </>
   );
 }
