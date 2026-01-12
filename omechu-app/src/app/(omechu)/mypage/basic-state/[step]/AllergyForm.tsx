8e541ab463b9d894bcc3e096662a78b4c55d6cc4
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-13 03:44:06 +0900

    [#220]feat:BaseModal 변경

--- omechu-app/src/app/(omechu)/mypage/basic-state/[step]/AllergyForm.tsx
+++ omechu-app/src/app/(omechu)/mypage/basic-state/[step]/AllergyForm.tsx
@@ -110,7 +110,6 @@ export default function AllergyForm() {
               setShowSaveModal(false);
               router.push("/mainpage");
             }}
-            // onRightButtonClick={() => }
           />
         </ModalWrapper>
       )}
