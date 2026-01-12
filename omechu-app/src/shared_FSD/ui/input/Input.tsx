218d354185ae5f8bf3c1e28544d8fb5347652155
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-12 23:50:37 +0900

    [#220]feat:EyeIcon.tsx 구현

--- omechu-app/src/shared_FSD/ui/input/Input.tsx
+++ omechu-app/src/shared_FSD/ui/input/Input.tsx
@@ -5,6 +5,12 @@ import Image from "next/image";
 
 import { cva, type VariantProps } from "class-variance-authority";
 import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import {
+  CloseEyeIcon,
+  OpenEyeIcon,
+} from "@/shared_FSD/assets/icons/ui/Input/EyeIcon";
 
 const inputStyles = cva(
   [
@@ -60,7 +66,7 @@ const PasswordInput = React.forwardRef<HTMLInputElement, BaseInputProps>(
           type={isVisible ? "text" : "password"}
           disabled={disabled}
           autoComplete="off"
-          className="flex-1 bg-transparent outline-none"
+          className="flex-1 bg-transparent pr-6 outline-none"
           value={props.value}
           onChange={props.onChange}
           {...props}
@@ -71,12 +77,11 @@ const PasswordInput = React.forwardRef<HTMLInputElement, BaseInputProps>(
           className="absolute right-3 flex items-center"
           aria-label="비밀번호 보기 전환"
         >
-          <Image
-            src={isVisible ? "/eye/eye_open.svg" : "/eye/eye_closed.svg"}
-            alt=""
-            width={20}
-            height={20}
-          />
+          {isVisible ? (
+            <OpenEyeIcon className="w-6" />
+          ) : (
+            <CloseEyeIcon className="w-6" />
+          )}
         </button>
       </div>
     );
@@ -140,7 +145,7 @@ export const Input = React.forwardRef<HTMLInputElement, BaseInputProps>(
         autoComplete="off"
         value={props.value}
         onChange={props.onChange}
-        className={clsx(
+        className={twMerge(
           inputStyles({
             width: props.width,
             height: props.height,
