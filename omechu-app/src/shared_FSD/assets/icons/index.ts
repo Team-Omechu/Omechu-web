ea517c0fa650b9e29807fb7d3c75f0069dcc3e13
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-13 03:57:02 +0900

    [#220]feat:HomeIcon 구현

--- omechu-app/src/shared_FSD/assets/icons/index.ts
+++ omechu-app/src/shared_FSD/assets/icons/index.ts
@@ -2,6 +2,7 @@ export { ArrowIcon } from "./mypage/ArrowIcon";
 export { CloseIcon } from "./mypage/CloseIcon";
 export { HistoryRoundedIcon } from "./mypage/HistoryRoundedIcon";
 export { WriteIcon } from "./mypage/WriteIcon";
+export { HomeIcon } from "./mypage/HomeIcon";
 
 export { OpenEyeIcon, CloseEyeIcon } from "./ui/input/EyeIcon";
 export { SearchIcon } from "./ui/input/SearchIcon";
---
+++ omechu-app/src/shared_FSD/assets/icons/mypage/HomeIcon.tsx
@@ -0,0 +1,27 @@
+interface HomeIconProps {
+  className?: string;
+}
+
+export const HomeIcon = ({ className }: HomeIconProps) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M3 11.5274C3 9.8679 3.77252 8.30292 5.08992 7.29368L8.75659 4.48472C10.6703 3.01862 13.3297 3.01862 15.2434 4.48472L18.9101 7.29368C20.2275 8.30292 21 9.8679 21 11.5274V15.6667C21 18.6122 18.6122 21 15.6667 21H8.33333C5.38782 21 3 18.6122 3 15.6667V11.5274Z"
+        stroke="#242424"
+        strokeWidth="2.4"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 21V15.75C14 14.6454 13.1046 13.75 12 13.75C10.8954 13.75 10 14.6454 10 15.75V21"
+        stroke="#242424"
+        strokeWidth="2.4"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
