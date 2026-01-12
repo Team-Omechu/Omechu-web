1f83cff95a6689abbc3e8fe2e19395034dfe186c
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-13 06:09:51 +0900

    [#220]feat:RecommendedFoodBox 컴포넌트 구현

--- omechu-app/src/app/example-testpage/mypage/recommended-list/page.tsx
+++ omechu-app/src/app/example-testpage/mypage/recommended-list/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRef, useState } from "react";
+<<<<<<< HEAD
 
 import { Header } from "@/shared/index";
 
@@ -58,6 +59,62 @@ export default function RecommendedListPage() {
         className="relative mt-2 flex h-[91.5dvh] flex-col items-center gap-5 overflow-y-auto"
       >
         <SelectTab
+<<<<<<< HEAD
+=======
+=======
+"use client";
+
+import { useState } from "react";
+
+import { Header } from "@/shared_FSD/index";
+
+import SelectTabBar from "../ui/SelectTabBar";
+=======
+
+import { Header } from "@/shared_FSD/index";
+
+import FloatingActionButton from "../ui/FloatingActionButton";
+import { RecommendedFoodBox } from "../ui/RecommendedFoodBox";
+import { SearchBar } from "../ui/SearchBar";
+import SelectTab from "../ui/SelectTab";
+>>>>>>> 15b9bdc6 ([#220]feat:RecommendedFoodBox 컴포넌트 구현)
+
+export default function RecommendedListPage() {
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+<<<<<<< HEAD
+  return (
+    <>
+      <Header title="추천 목록 관리" isRightChild={true} />
+      <main className="gap- relative mt-4 flex h-[91.5dvh] flex-col items-center px-5">
+        <SelectTabBar
+>>>>>>> ef4577ad ([#220]design:SelectTab 수정)
+=======
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleSearch = (term: string) => {
+    console.log("검색 실행:", term);
+  };
+
+  const scrollToTop = () => {
+    if (mainRef.current) {
+      mainRef.current.scrollTo({ top: 0, behavior: "smooth" });
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+  return (
+    <>
+      <Header title="추천 목록 관리" isRightChild={true} />
+      <main
+        ref={mainRef}
+        className="relative mt-2 flex h-[91.5dvh] flex-col items-center gap-5 overflow-y-auto"
+      >
+        <SelectTab
+>>>>>>> 15b9bdc6 ([#220]feat:RecommendedFoodBox 컴포넌트 구현)
+>>>>>>> c47c37b0 ([#220]feat:RecommendedFoodBox 컴포넌트 구현)
           tabs={["추천 목록", "제외 목록"]}
           selectedIndex={selectedIndex}
           onSelect={setSelectedIndex}
@@ -66,22 +123,41 @@ export default function RecommendedListPage() {
           inputValue={searchTerm}
           setInputValue={setSearchTerm}
           onSearch={handleSearch}
+<<<<<<< HEAD
           suggestionList={MENU_SUGGESTIONS}
         />
 
         <section className="grid w-84 grid-cols-3 gap-3 pb-15">
           {filteredItems.map((i) => (
+=======
+        />
+        <section className="grid w-84 grid-cols-3 gap-3 pb-15">
+          {Array.from({ length: 30 }).map((_, i) => (
+>>>>>>> 15b9bdc6 ([#220]feat:RecommendedFoodBox 컴포넌트 구현)
             <RecommendedFoodBox
               key={i}
               title={`타코 ${i}`}
               src=""
+<<<<<<< HEAD
               onClick={() => toggleExclude(i)}
               isToggled={excludedSet.has(i)}
+=======
+              onClick={() => setIsToggled(!isToggled)}
+              isToggled={isToggled}
+>>>>>>> 15b9bdc6 ([#220]feat:RecommendedFoodBox 컴포넌트 구현)
             />
           ))}
         </section>
 
         <FloatingActionButton onClick={scrollToTop} />
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> ef4577ad ([#220]design:SelectTab 수정)
+=======
+>>>>>>> 15b9bdc6 ([#220]feat:RecommendedFoodBox 컴포넌트 구현)
+>>>>>>> c47c37b0 ([#220]feat:RecommendedFoodBox 컴포넌트 구현)
       </main>
     </>
   );
