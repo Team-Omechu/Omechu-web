8889973c6097649852d39cbcb06aff47995598ef
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-19 23:41:41 +0900

    [#244]fix: 동일 모듈 내 import 절대 경로 -> 상대 경로로 변경

--- omechu-app/src/widgets/mypage/ui/index.ts
+++ omechu-app/src/widgets/mypage/ui/index.ts
@@ -1,20 +1,20 @@
-export { CustomerSupportSection } from "@/widgets/mypage/ui/CustomerSupportSection";
-export { CustomDatePicker } from "@/widgets/mypage/ui/DatePicker/CustomDatePicker";
-export { CustomInput } from "@/widgets/mypage/ui/DatePicker/CustomInput";
-export { DatePickerHeader } from "@/widgets/mypage/ui/DatePicker/DatePickerHeader";
-export { FloatingActionButton } from "@/widgets/mypage/ui/FloatingActionButton";
-export { MukburimFoodBox } from "@/widgets/mypage/ui/MukburimFoodBox";
-export { PeriodTap } from "@/widgets/mypage/ui/PeriodTap";
-export { RecommendedFoodBox } from "@/widgets/mypage/ui/RecommendedFoodBox";
+export { CustomerSupportSection } from "./CustomerSupportSection";
+export { CustomDatePicker } from "./DatePicker/CustomDatePicker";
+export { CustomInput } from "./DatePicker/CustomInput";
+export { DatePickerHeader } from "./DatePicker/DatePickerHeader";
+export { FloatingActionButton } from "./FloatingActionButton";
+export { MukburimFoodBox } from "./MukburimFoodBox";
+export { PeriodTap } from "./PeriodTap";
+export { RecommendedFoodBox } from "./RecommendedFoodBox";
 
-export { SelectTab } from "@/widgets/mypage/ui/SelectTab";
-export { SetAlarmSection } from "@/widgets/mypage/ui/SetAlarmSection";
-export { UserInfoSection } from "@/widgets/mypage/ui/UserInfoSection";
+export { SelectTab } from "./SelectTab";
+export { SetAlarmSection } from "./SetAlarmSection";
+export { UserInfoSection } from "./UserInfoSection";
 
-export { TimePickerModal } from "@/widgets/mypage/ui/TimePickerModal";
-export { ToggleSwitch } from "@/widgets/mypage/ui/ToggleSwitch";
+export { TimePickerModal } from "./TimePickerModal";
+export { ToggleSwitch } from "./ToggleSwitch";
 
 // Terms : 이용약관
-export { TermForService } from "@/widgets/mypage/ui/terms/TermForService";
-export { TermForPersonalInfo } from "@/widgets/mypage/ui/terms/TermForPersonalInfo";
-export { TermForLocationInfo } from "@/widgets/mypage/ui/terms/TermForLocationInfo";
+export { TermForService } from "./terms/TermForService";
+export { TermForPersonalInfo } from "./terms/TermForPersonalInfo";
+export { TermForLocationInfo } from "./terms/TermForLocationInfo";
