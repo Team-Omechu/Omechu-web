de14d50b5d4f5656632eb7dd8c646cb3bf30a141
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-21 05:14:28 +0900

    [#251]remove:/entities/mypage/model/*.ts 파일 삭제

--- omechu-app/src/entities/mypage/index.ts
+++ omechu-app/src/entities/mypage/index.ts
@@ -2,16 +2,3 @@
 // entities/mypage 배럴 export
 // FSD 규칙: entities는 shared만 의존
 // ============================================
-
-// Model
-export {
-  type UpdateProfilePayload,
-  type CompleteProfilePayload,
-  type ProfileLike,
-  buildUpdatePayloadFromStore,
-  buildCompletePayloadFromStore,
-} from "./model/profilePayload";
-export { type MyPlaceApiResponseType } from "./model/MyPlaceApiResponseType";
-export { useProfile } from "./model/useProfile";
-export { useProfileQuery } from "./model/useProfileQuery";
-export { resetBasicStateAndSync } from "./model/resetBasicState";
