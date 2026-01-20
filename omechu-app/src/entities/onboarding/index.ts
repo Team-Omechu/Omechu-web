e4db1862ff40cae0d04b58b6ce507513be054825
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-21 05:02:22 +0900

    [#251]remove:/entities/onboarding/ui/ExerciseStep.tsx 파일 삭제

--- omechu-app/src/entities/onboarding/index.ts
+++ omechu-app/src/entities/onboarding/index.ts
@@ -15,7 +15,6 @@ export { useCompleteOnboardingMutation } from "./model/useOnboarding";
 
 // UI
 
-export { default as ExerciseStep } from "./ui/ExerciseStep";
 export { default as GenderStep } from "./ui/GenderStep";
 export { default as OnboardingStepLayout } from "./ui/OnboardingStepLayout";
 export { default as PreferStep } from "./ui/PreferStep";
