00e770c52c20fdfeedbe38aa1da2de955d4e4fdf
IISweetHeartII <sachi009955@gmail.com>
2026-01-22 01:37:16 +0900

    feat: 소셜 로그인 UI를 SDK 방식으로 전환 (#254)

--- omechu-app/src/app/(auth)/sign-in/page.tsx
+++ omechu-app/src/app/(auth)/sign-in/page.tsx
@@ -1,40 +1,50 @@
+"use client";
+
 import Link from "next/link";
 
+import { useGoogleLogin, useKakaoLogin } from "@/entities/user";
 import { AuthButton } from "@/shared";
+import { Toast, useToast } from "@/shared";
 
 /**
  * 소셜 로그인 메인 페이지
- * - 카카오, 구글 소셜 로그인 버튼
+ * - 카카오, 구글 SDK 기반 소셜 로그인 버튼
  * - 이메일 로그인/회원가입 링크
  */
 export default function SignInPage() {
+  const { initiateKakaoLogin, isLoading: kakaoLoading } = useKakaoLogin();
+  const { handleGoogleLogin, isLoading: googleLoading } = useGoogleLogin();
+  const { show, message } = useToast();
+
   return (
     <div className="flex flex-col items-center">
       {/* 로그인 버튼 영역 */}
       <div className="mt-20 flex w-80 flex-col gap-6">
         {/* 카카오 로그인 */}
-        <a href={`${process.env.NEXT_PUBLIC_API_URL}/auth/kakao`}>
-          <AuthButton
-            variant="kakao"
-            icon="/kakao/kakao.svg"
-            iconAlt="카카오 아이콘"
-            type="button"
-          >
-            카카오 로그인
-          </AuthButton>
-        </a>
+        <AuthButton
+          variant="kakao"
+          icon="/kakao/kakao.svg"
+          iconAlt="카카오 아이콘"
+          type="button"
+          onClick={initiateKakaoLogin}
+          isLoading={kakaoLoading}
+          disabled={kakaoLoading || googleLoading}
+        >
+          카카오 로그인
+        </AuthButton>
 
         {/* 구글 로그인 */}
-        <a href={`${process.env.NEXT_PUBLIC_API_URL}/auth/google`}>
-          <AuthButton
-            variant="google"
-            icon="/google/google.svg"
-            iconAlt="구글 아이콘"
-            type="button"
-          >
-            구글로 로그인
-          </AuthButton>
-        </a>
+        <AuthButton
+          variant="google"
+          icon="/google/google.svg"
+          iconAlt="구글 아이콘"
+          type="button"
+          onClick={handleGoogleLogin}
+          isLoading={googleLoading}
+          disabled={kakaoLoading || googleLoading}
+        >
+          구글로 로그인
+        </AuthButton>
       </div>
 
       {/* 이메일 로그인 / 회원가입 링크 */}
@@ -50,6 +60,8 @@ export default function SignInPage() {
           이메일 회원가입
         </Link>
       </div>
+
+      <Toast message={message} show={show} />
     </div>
   );
 }
