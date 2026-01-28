9dda67c8fb6ae2432d57b88ac48bec3fc63ee1f5
Rael <emilyekos@gmail.com>
2026-01-29 07:59:41 +0900

    chore: 소켓 및 API 관련 타입 export 추가 #200

--- omechu-app/src/entities/menubattle/index.ts
+++ omechu-app/src/entities/menubattle/index.ts
@@ -1,3 +1,9 @@
 export { calculateMenuResult } from "./model/calc";
 export { ResultPanel } from "./ui/ResultPanel";
 export type { Menu, Player, PlayerResult } from "./model/types";
+export type {
+  ParticipantJoinedPayload,
+  SpinCompletedPayload,
+  BattleFinishedPayload,
+} from "./model/socket";
+export type { BattleResponse } from "./model/api";
